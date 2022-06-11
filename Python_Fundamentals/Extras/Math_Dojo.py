53437aba2f480853b7037c7a6a51d0a2a9d00dac
Matthew Luk <Matthew.Luk@yahoo.com>
2022-06-10 21:37:05 -0700

    first commit

---
+++ Python_Fundamentals/Extras/Math_Dojo.py
@@ -0,0 +1,19 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        for i in nums:
+            self.result = self.result + i
+        self.result = self.result + num
+        return(self)
+    def subtract(self, num, *nums):
+        for i in nums:
+            self.result = self.result - i
+        self.result = self.result - num
+        return(self)
+
+md = MathDojo()
+
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x) # should print 5
