7cbfc046621333ea0abc1290e00eab3bf1c891f9
Matthew Luk <Matthew.Luk@yahoo.com>
2022-07-01 08:34:53 -0700

    first commit

--- Flask/checkerboard/server.py
+++ Flask/checkerboard/server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def default():
     return render_template("index.html")
 
+@app.route('/<int:num>')
+def route(num):
+    return render_template("index.html", num=num)
+
+@app.route('/<int:x>/<int:y>')
+def two(x,y):
+    return render_template("index.html", row=x, col=y)
+
 if __name__=="__main__":
     app.run(debug=True)
