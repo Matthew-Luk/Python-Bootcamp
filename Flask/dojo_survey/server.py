05c8e48a375c42ee53d379ce6b3d99c4d84b091b
Matthew Luk <Matthew.Luk@yahoo.com>
2022-07-03 18:49:27 -0700

    first commit

---
+++ Flask/dojo_survey/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key =  "matthew1"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods = ["POST"])
+def process():
+    session["name"] = request.form["name"]
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comment"] = request.form["comment"]
+    return redirect('/results')
+
+@app.route('/results')
+def results():
+    return render_template("results.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
