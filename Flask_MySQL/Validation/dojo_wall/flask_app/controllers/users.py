e80adf0e44126eb60bd3da383bd43729cc58d354
Matthew Luk <Matthew.Luk@yahoo.com>
2022-07-20 21:26:42 -0700

    first commit

---
+++ Flask_MySQL/Validation/dojo_wall/flask_app/controllers/users.py
@@ -0,0 +1,70 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.user import User
+from flask_app.models.posts import Post
+from flask_app import app
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    if not User.validate_login(request.form):
+        return redirect("/")
+    user = User.get_by_email(request.form)
+    if user:
+        if not bcrypt.check_password_hash(user.password, request.form["password"]):
+            flash("The password you've entered is incorrect.")
+            return redirect("/")
+        session["user_id"] = user.id
+        return redirect("/dashboard")
+    flash("The email you entered isn't connected to an account. Find your account and log in.")
+    return redirect("/")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not User.validate_user(request.form):
+        return redirect("/")
+    pw_hash = bcrypt.generate_password_hash(request.form["password"])
+    data = {
+        "first_name":request.form["first_name"],
+        "last_name":request.form["last_name"],
+        "email":request.form["email"],
+        "password":pw_hash
+    }
+    user_id = User.save(data)
+    session["user_id"] = user_id
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    data = {
+        "id":session["user_id"]
+    }
+    return render_template("dashboard.html", user=User.get_by_id(data), all_posts = Post.posts_with_user(), session_id = session["user_id"])
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/dashboard/post", methods=["POST"])
+def add_post():
+    if not Post.validate_post(request.form):
+        return redirect("/dashboard")
+    data = {
+        "content":request.form["content"],
+        "user_id":session["user_id"]
+    }
+    Post.save(data)
+    return redirect("/dashboard")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    data = {
+        "id":id
+    }
+    Post.delete(data)
+    return redirect("/dashboard")
