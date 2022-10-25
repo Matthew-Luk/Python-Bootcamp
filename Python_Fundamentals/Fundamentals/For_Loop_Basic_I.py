29b2acfb0a34622eb50efb031db0b1c3d9e0b5b8
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-24 20:48:18 -0700

    fixed last function

--- Python_Fundamentals/Fundamentals/For_Loop_Basic_I.py
+++ Python_Fundamentals/Fundamentals/For_Loop_Basic_I.py
@@ -22,7 +22,6 @@ for i in range(1,101):
 count = 0
 for i in range(1,500000,2):
     count += i
-
 print(count)
 
 
@@ -35,5 +34,6 @@ for i in range (2018,0,-4):
 lowNum = 2
 highNum = 20
 mult =  3
-for i in range(lowNum,highNum,mult):
-    print(i)
+for i in range(lowNum,highNum+1):
+    if(i % mult == 0):
+        print(i)
