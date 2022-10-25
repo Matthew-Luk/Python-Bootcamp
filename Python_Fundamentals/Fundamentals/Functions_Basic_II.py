610bd2e813fd7c92d4115780fc975f3b085d2676
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-25 13:21:55 -0700

    fixed some functions

--- Python_Fundamentals/Fundamentals/Functions_Basic_II.py
+++ Python_Fundamentals/Fundamentals/Functions_Basic_II.py
@@ -1,8 +1,9 @@
 # Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
 def countdown(element):
     newList = []
-    for i in range(element,0,-1):
-        newList.append(i)
+    while(element >= 0):
+        newList.append(element)
+        element -= 1
     return newList
 
 print(countdown(5))
@@ -26,6 +27,8 @@ print(first_plus_length([1,2,3,4,5]))
 # Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 def values_greater_than_second(arr):
     newList = []
+    if(len(arr) < 2):
+        return False
     for i in arr:
         if i > arr[1]:
             newList.append(i)
@@ -33,6 +36,7 @@ def values_greater_than_second(arr):
     return newList
 
 print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
 
 
 # Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
