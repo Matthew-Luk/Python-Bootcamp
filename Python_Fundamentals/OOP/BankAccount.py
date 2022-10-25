f452ea10a5ba3df415417293b6096800347ccd44
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-25 14:10:51 -0700

    update methods and default parameters

--- Python_Fundamentals/OOP/BankAccount.py
+++ Python_Fundamentals/OOP/BankAccount.py
@@ -1,6 +1,6 @@
 class BankAccount:
     accounts = []
-    def __init__(self, int_rate, balance): 
+    def __init__(self, int_rate, balance=0): 
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.accounts.append(self)
@@ -13,12 +13,11 @@ class BankAccount:
         if amount > self.balance:
             print("Insufficient funds: Charging a $5 fee")
             self.balance = (self.balance - 5)
-        else:
-            self.balance = (self.balance - amount)
+        self.balance = (self.balance - amount)
         return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"Balance: ${self.balance}, Interest Rate: {self.int_rate * 100}%")
         return self
 
     def yield_interest(self):
@@ -30,7 +29,8 @@ class BankAccount:
         for account in cls.accounts:
             account.display_account_info()
 
-account1 = BankAccount(0.05, 500)
+account1 = BankAccount(0.05)
+account1.display_account_info()
 account2 = BankAccount(0.03, 1000)
 account1.deposit(50).deposit(70).deposit(30).withdraw(20).yield_interest().display_account_info()
 account2.deposit(100).deposit(75).withdraw(10).withdraw(200).withdraw(50).withdraw(25).yield_interest().display_account_info()
