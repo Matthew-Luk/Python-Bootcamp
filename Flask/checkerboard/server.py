5ed915892392cc319752b3d5567725b5feeab1fc
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-27 11:11:21 -0700

    added and updated routes

--- Flask/checkerboard/server.py
+++ Flask/checkerboard/server.py
@@ -3,16 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def default():
-    return render_template("index.html")
+def route1():
+    return render_template("index.html", row=8, col=8, color1="red", color2="black")
 
 @app.route('/<int:num>')
-def route(num):
-    return render_template("index.html", num=num)
+def route2(num):
+    return render_template("index.html", row=num, col=8, color1="red", color2="black")
 
 @app.route('/<int:x>/<int:y>')
-def two(x,y):
-    return render_template("index.html", row=x, col=y)
+def route3(x,y):
+    return render_template("index.html", row=x, col=y, color1="red", color2="black")
+
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def route4(x,y,color1,color2):
+    return render_template("index.html", row=x, col=y, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
