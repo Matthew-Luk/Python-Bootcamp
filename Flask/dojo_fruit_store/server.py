d33ec125b042ddb6a136b0a306be2e34a5e8efef
Matthew Luk <Matthew.Luk@yahoo.com>
2022-10-28 17:18:18 -0700

    updated route to pass more variables and time

--- Flask/dojo_fruit_store/server.py
+++ Flask/dojo_fruit_store/server.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from datetime import date
 app = Flask(__name__)  
 
-@app.route('/')         
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    today = date.today()
+    d1 = today.strftime("%B %d, %Y")
+    total = (int(request.form["strawberry"]) + int(request.form["raspberry"]) + int(request.form["apple"]) + int(request.form["blackberry"]))
+    return render_template("checkout.html", d1=d1, total=total)
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)   
+if __name__=="__main__":
+    app.run(debug=True)
