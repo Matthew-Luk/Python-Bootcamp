061d4ccae5d7967bf1f72f714d9468eb08f4079e
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-02 20:46:07 -0700

    updated route to include activities list

--- Flask/ninja_gold/server.py
+++ Flask/ninja_gold/server.py
@@ -1,4 +1,6 @@
 import random
+import datetime
+import time
 from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)  
@@ -8,39 +10,39 @@ app.secret_key = "matthew1"
 def index():
     if 'gold' not in session:
         session['gold'] = 0
-    if 'activities_list' in session:
-        print(session['activities_list'])
-    return render_template('index.html', gold=session['gold'], activities=session['activities_list'])
+    if 'activities_list' not in session:
+        session['activities_list'] = []
+    if 'attempts' not in session:
+        session['attempts'] = 0
+    print(session['attempts'])
+    return render_template('index.html', gold=session['gold'], activities=session['activities_list'], attempts=session['attempts'])
 
 @app.route('/process_money', methods=["post"])
 def process_money():
-    session['activities_list'] = []
+    x = time.localtime()
+    date = time.strftime("%Y/%m/%d, %I:%M:%p", x)
     if request.form['building'] == "farm":
         a = (random.randint(10,20))
-        print(a)
-        session['activities_list'].append(f"Earned {a} golds from the farm!")
+        session['activities_list'].append(f"Earned {a} golds from the farm! ({date})")
         session['gold'] = session['gold'] + a
     elif request.form['building'] == "cave":
         b = (random.randint(5,10))
-        print(b)
-        session['activities_list'].append(f"Earned {b} golds from the cave!")
+        session['activities_list'].append(f"Earned {b} golds from the cave! ({date})")
         session['gold'] = session['gold'] + b
     elif request.form['building'] == "house":
         c = (random.randint(2,5))
-        print(c)
-        session['activities_list'].append(f"Earned {c} golds from the house!")
+        session['activities_list'].append(f"Earned {c} golds from the house! ({date})")
         session['gold'] = session['gold'] + c
-    else:
+    elif request.form['building'] == "casino":
         x = random.randint(1,2)
         y = (random.randint(0,50))
-        print(x)
-        print(y)
         if x == 1:
             session['gold'] = session['gold'] + y
-            session['activities_list'].append(f"Entered a casino and earned {y} golds!")
+            session['activities_list'].append(f"Entered a casino and earned {y} golds! ({date})")
         elif x == 2:
             session['gold'] = session['gold'] - y
-            session['activities_list'].append(f"Entered a casino and lost {y} golds... Ouch..")
+            session['activities_list'].append(f"Entered a casino and lost {y} golds... Ouch.. ({date})")
+    session['attempts'] += 1
     return redirect('/')
 
 @app.route('/reset')
