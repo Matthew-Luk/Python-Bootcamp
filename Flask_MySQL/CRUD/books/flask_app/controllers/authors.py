df45e1f1ba0b72d0dd02ba7bf2ac50dec1cbe5b9
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-07 00:06:24 -0800

    update routes

--- Flask_MySQL/CRUD/books/flask_app/controllers/authors.py
+++ Flask_MySQL/CRUD/books/flask_app/controllers/authors.py
@@ -9,10 +9,7 @@ def index():
 
 @app.route("/authors")
 def authors():
-    data = {
-        "id":1
-    }
-    return render_template("authors.html", all_authors = Author.get_all(), author = Author.get_by_id(data))
+    return render_template("authors.html", all_authors = Author.get_all())
 
 @app.route("/create_author", methods = ["POST"])
 def create_author():
@@ -24,4 +21,9 @@ def author_show(id):
     data = {
         "id":id
     }
-    return render_template("author_show.html", author = Author.get_by_id(data))
+    return render_template("author_show.html", author = Author.get_by_id(data), all_books = Book.get_all())
+
+@app.route("/add_favorite_book", methods=["POST"])
+def add_favorite_book():
+    Author.add_to_favorites(request.form)
+    return redirect('/')
