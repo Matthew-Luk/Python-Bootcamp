85b249951739aeacdea57e7e895d722885ab9bf5
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-07 00:07:15 -0800

    update routes

--- Flask_MySQL/CRUD/books/flask_app/controllers/books.py
+++ Flask_MySQL/CRUD/books/flask_app/controllers/books.py
@@ -6,4 +6,16 @@ from flask_app import app
 
 @app.route("/add_book")
 def add_book():
-    pass
+    return render_template("books.html", all_books = Book.get_all())
+
+@app.route("/add_book_submit", methods = ["POST"])
+def add_book_submit():
+    Book.save(request.form)
+    return redirect("/add_book")
+
+@app.route("/book_show/<int:id>")
+def book_show(id):
+    data = {
+        "id":id
+    }
+    return render_template("book_show.html", book = Book.get_by_id(data), all_authors = Author.get_all())
