90b0139dd0f6964be2557a788e617de0401dab07
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-09 20:47:52 -0800

    update model with new schema

--- Flask_MySQL/Validation/email_validation/flask_app/models/user.py
+++ Flask_MySQL/Validation/email_validation/flask_app/models/user.py
@@ -5,16 +5,18 @@ from flask import flash
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
-    db = "email"
+    db = "email_validation"
     def __init__(self,data):
         self.id = data["id"]
-        self.email_address = data["email_address"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
 
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM emails;"
+        query = "SELECT * FROM users;"
         results = connectToMySQL(cls.db).query_db(query)
         user = []
         for row in results:
@@ -23,24 +25,40 @@ class User:
     
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO emails(email_address) VALUES(%(email_address)s);"
+        query = "INSERT INTO users(first_name,last_name,email) VALUES(%(first_name)s,%(last_name)s,%(email)s);"
         return connectToMySQL(cls.db).query_db(query,data)
     
     @classmethod
     def delete(cls,data):
-        query = "DELETE FROM emails WHERE id = %(id)s;"
+        query = "DELETE FROM users WHERE id = %(id)s;"
         return connectToMySQL(cls.db).query_db(query,data)
 
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        print(f'Here are the results:{results}')
+        if len(results) < 1:
+            return False
+        row = results[0]
+        user = cls(row)
+        return user
+
     @staticmethod
-    def validate_email(email):
-        print("Hello")
+    def validate_user(user):
         is_valid = True
-        query = "SELECT * FROM emails WHERE email_address = %(email_address)s;"
-        results = connectToMySQL(User.db).query_db(query,email)
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(User.db).query_db(query,user)
         if len(results) > 0:
             flash("Email has already been entered.")
             is_valid = False
-        if not EMAIL_REGEX.match(email["email_address"]):
+        if not EMAIL_REGEX.match(user["email"]):
             flash("Invalid Email Address")
+            is_valid = False
+        if len(user["first_name"]) < 3:
+            flash("First name must be at least 3 characters.")
+            is_valid = False
+        if len(user["last_name"]) < 3:
+            flash("Last name must be at least 3 characters.")
             is_valid = False
         return is_valid
