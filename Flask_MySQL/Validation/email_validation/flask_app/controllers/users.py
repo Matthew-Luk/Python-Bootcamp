0dce7297c73af297d24eaf45e619d32b857876cc
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-09 20:47:38 -0800

    update routes with session

--- Flask_MySQL/Validation/email_validation/flask_app/controllers/users.py
+++ Flask_MySQL/Validation/email_validation/flask_app/controllers/users.py
@@ -4,19 +4,28 @@ from flask_app import app
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'first_name' not in session:
+        session['first_name'] = ""
+    if 'last_name' not in session:
+        session['last_name'] = ""
+    if 'email' not in session:
+        session['email'] = ""
+    return render_template("index.html", first_name = session['first_name'], last_name = session['last_name'], email = session['email'])
 
 @app.route("/process",methods=["POST"])
 def process():
-    print("Hello")
-    if not User.validate_email(request.form):
+    if not User.validate_user(request.form):
+        session["first_name"] = request.form["first_name"]
+        session["last_name"] = request.form["last_name"]
+        session["email"] = request.form["email"]
         return redirect("/")
     User.save(request.form)
     return redirect("/success")
 
 @app.route("/success")
 def success():
-    return render_template("success.html", emails = User.get_all())
+    session.clear()
+    return render_template("success.html", all_users = User.get_all())
 
 @app.route("/success/delete/<int:id>")
 def delete(id):
