d030209b5f96ee3791a0281c5ed1dc12aaaa4c08
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-12 11:52:49 -0800

    delete uneeded imports and separated controllers

--- Flask_MySQL/Validation/dojo_wall/flask_app/controllers/users.py
+++ Flask_MySQL/Validation/dojo_wall/flask_app/controllers/users.py
@@ -1,7 +1,5 @@
-from crypt import methods
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
-from flask_app.models.posts import Post
 from flask_app import app
 from flask_bcrypt import Bcrypt
 
@@ -39,33 +37,7 @@ def register():
     session["user_id"] = user_id
     return redirect("/dashboard")
 
-@app.route("/dashboard")
-def dashboard():
-    data = {
-        "id":session["user_id"]
-    }
-    return render_template("dashboard.html", user=User.get_by_id(data), all_posts = Post.posts_with_user(), session_id = session["user_id"])
-
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
-
-@app.route("/dashboard/post", methods=["POST"])
-def add_post():
-    if not Post.validate_post(request.form):
-        return redirect("/dashboard")
-    data = {
-        "content":request.form["content"],
-        "user_id":session["user_id"]
-    }
-    Post.save(data)
-    return redirect("/dashboard")
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    data = {
-        "id":id
-    }
-    Post.delete(data)
-    return redirect("/dashboard")
