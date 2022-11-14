027754a177b0ae7117e9b8f6e29edfda52610fbf
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-13 17:09:39 -0800

    deleted uneeded imports

--- Flask_MySQL/Belt_Review/recipes/flask_app/models/user.py
+++ Flask_MySQL/Belt_Review/recipes/flask_app/models/user.py
@@ -1,6 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-from flask_bcrypt import Bcrypt
 import re
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
